5c3c029c70cc4474ca5bef6b6eef03ba846ca881
OSMAR SF <71989728+OSMAR2910@users.noreply.github.com>
2022-12-07 18:11:58 -0800

    Add files via upload

---
+++ Game/lib/index.js
@@ -0,0 +1,46 @@
+import DinoGame from './game/DinoGame.js'
+
+alert('Te recomendamos que pongas tu dispositivo móvil en horizontal')
+
+const game = new DinoGame(900, 200)
+const isTouchDevice =
+  'ontouchstart' in window ||
+  navigator.maxTouchPoints > 0 ||
+  navigator.msMaxTouchPoints > 0
+
+if (isTouchDevice) {
+  document.addEventListener('touchstart', ({ touches }) => {
+    if (touches.length === 1) {
+      game.onInput('jump')
+    } else if (touches.length === 2) {
+      game.onInput('duck')
+    }
+  })
+
+  document.addEventListener('touchend', ({ touches }) => {
+    game.onInput('stop-duck')
+  })
+} else {
+  const keycodes = {
+    // up, spacebar
+    JUMP: { 38: 1, 32: 1 },
+    // down
+    DUCK: { 40: 1 },
+  }
+
+  document.addEventListener('keydown', ({ keyCode }) => {
+    if (keycodes.JUMP[keyCode]) {
+      game.onInput('jump')
+    } else if (keycodes.DUCK[keyCode]) {
+      game.onInput('duck')
+    }
+  })
+
+  document.addEventListener('keyup', ({ keyCode }) => {
+    if (keycodes.DUCK[keyCode]) {
+      game.onInput('stop-duck')
+    }
+  })
+}
+
+game.start().catch(console.error)
