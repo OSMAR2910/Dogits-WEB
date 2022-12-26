d7de70896322b511e110780b7727aba8c531c625
OSMAR SF <71989728+OSMAR2910@users.noreply.github.com>
2022-12-26 01:10:18 -0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,64 @@
+//Crarga
+window.onload = function () {
+    var contenedor = document.getElementById('logo_load');
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
+//Aniscroll
+const info = document.getElementById("info");
+const serv = document.getElementById("serv");
+const gale = document.getElementById("gale");
+const cont = document.getElementById("cont");
+
+const cargarImagen = (entradas, observador) => {
+
+    entradas.forEach((entrada) => {
+        if (entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        } else {
+            entrada.target.classList.remove('visible');
+        }
+    });
+}
+
+const observador  = new IntersectionObserver(cargarImagen, {
+    root: null,
+    rootMargin: '400px 0px 300px 0px',
+    threshold: 1.0
+});
+
+observador.observe(info);
+observador.observe(serv);
+observador.observe(gale);
+observador.observe(cont);
+//Modal1
+const abrirmodal = document.querySelector("#button_ag_abrir");
+const cerrarmodal = document.querySelector("#button_ag_cerrar");
+const modal = document.querySelector("#modal1");
+
+abrirmodal.addEventListener("click",()=>{
+    modal.showModal();
+}
+)
+cerrarmodal.addEventListener("click",()=>{
+    modal.close();
+}
+)
+//QR
+const code = document.getElementById('code');
+
+new QRCode(code, 'No ahi informacion ahun');
+//Playvid
+var fondo = document.getElementById("vid");
+var sound = document.getElementById("aud");
+function playPause() {
+    if(sound.paused)
+          sound.play();
+          else
+          sound.pause();
+    if(fondo.paused)
+          fondo.play();
+          else
+          fondo.pause();
+          
+}
